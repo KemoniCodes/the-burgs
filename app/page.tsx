d96978c30f63c8cadf33d656b64dce8a17fd1026
kemonicodes22 <kemoniw333@gmail.com>
2025-05-14 19:59:45 -0700

    section 2

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,24 @@ export default function Home() {
     <div className='home'>
       <Hero />
       <div className='section2 border-y-black border-y-[7px] border-y-solid py-[175px] px-[152px]'>
-        <Image src={GreenBadge} width={250} height={250} alt='green badge' className="absolute right-0 top-[100vh]"/>
+        <Image
+          src={GreenBadge}
+          width={250}
+          height={250}
+          alt='green badge'
+          className='absolute right-0 top-[100vh]'
+        />
         <h1 className='text-center text-retroBlue'>
           Your friendly neighborhood burger joint. 100% grass-fed beef brought
           to you by your pals at The ‘Burgs.
         </h1>
-        <Image src={PinkLogo} width={276} height={177} alt='pink logo' className="absolute left-20 mt-20"/>
+        <Image
+          src={PinkLogo}
+          width={276}
+          height={177}
+          alt='pink logo'
+          className='absolute left-20 mt-20'
+        />
       </div>
     </div>
   );
