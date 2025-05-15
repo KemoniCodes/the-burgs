12ef9aa3cab18811ebf367188669183b441ba940
kemonicodes22 <kemoniw333@gmail.com>
2025-05-14 19:32:06 -0700

    banner

--- app/components/home/banner.tsx
+++ app/components/home/banner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Banner() {
   return (
-    <div className='banner top-0 relative bg-bunBeige h-[56px] flex justify-center items-center'>
+    <div className='banner top-0 relative bg-bunBeige h-[74px] flex justify-center items-center'>
       <h4 className='text-retroBlue text-center'>
         GRAND OPENING 8/22 LOS ANGELES, CA!
       </h4>
